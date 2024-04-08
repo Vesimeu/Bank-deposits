f2765050ed1184bf82e8eb8795778d8b0edc955e
Vesimeu <pyoter.poliakov@yandex.ru>
2024-04-08 15:20:22 +0500

    Last fix from BD:

--- demo/src/main/java/com/example/bankdeposit/config/DatabaseConfig.java
+++ demo/src/main/java/com/example/bankdeposit/config/DatabaseConfig.java
@@ -1,14 +1,22 @@
 package com.example.bankdeposit.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.annotation.PostConstruct;
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Properties;
 
 @Configuration
@@ -16,20 +24,23 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class DatabaseConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(DatabaseConfig.class);
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://YOUR_DB_URL:3306/your_database_name?useSSL=false&serverTimezone=UTC");
-        dataSource.setUsername("YOUR_DB_USERNAME");
-        dataSource.setPassword("YOUR_DB_PASSWORD");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/bank_deposit?useSSL=false&serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("123123");
+
         return dataSource;
     }
 
     @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
         LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
-        em.setDataSource(dataSource());
+        em.setDataSource(dataSource);
         em.setPackagesToScan("com.example.bankdeposit.model");
 
         Properties properties = new Properties();
@@ -45,5 +56,17 @@ public class DatabaseConfig {
         JpaTransactionManager transactionManager = new JpaTransactionManager();
         transactionManager.setEntityManagerFactory(emf);
         return transactionManager;
+    }
+
+    @Autowired
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void checkDatabaseConnection() {
+        try (Connection connection = dataSource.getConnection()) {
+            logger.info("Database connection successful!");
+        } catch (SQLException e) {
+            logger.error("Database connection failed:", e);
+        }
     }
 }
